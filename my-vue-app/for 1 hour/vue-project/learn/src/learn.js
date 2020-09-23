1c10834167edbc1c21c13519a748c6b4e32b7b4b
Efim Zabarsky <YAFIMZ@rf.local>
2020-09-23 10:32:08 +0300

    rfct

---
+++ my-vue-app/for 1 hour/vue-project/learn/src/learn.js
@@ -0,0 +1,3 @@
+export default{
+    i: 7106
+}
--- my-vue-app/for 1 hour/vue-project/learn/src/main.js
+++ my-vue-app/for 1 hour/vue-project/learn/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Learn from './learn'
 
 Vue.config.productionTip = false
-
+console.log(Learn.i)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
